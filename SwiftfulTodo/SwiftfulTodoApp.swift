e1b4000d64f034b6bf248cbcda8f7f781a4de278
Jiten Patel <thejitenpatel@gmail.com>
2023-06-23 20:32:21 +0530

    Initial Commit

---
+++ SwiftfulTodo/SwiftfulTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulTodoApp.swift
+//  SwiftfulTodo
+//
+//  Created by Jiten Patel on 23/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
